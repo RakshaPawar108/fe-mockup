eb288e7d6812585e4fc9064a98fad0516024721e
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-09-02 13:48:32 +0530

    chore: add check mark.png image

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,6 @@
 import edenLogo from "./eden-logo.svg";
 import userOne from "./user-one.png";
 import usersMany from "./users-many.png";
+import checkMarkImg from './check-mark.png'
 
-export { edenLogo, userOne, usersMany };
+export { edenLogo, userOne, usersMany, checkMarkImg };
